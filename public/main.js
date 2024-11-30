ad354f33537fb0c27d3b1b7462c256d446dffaf1
mendsxyz <princemendie03@gmail.com>
2024-11-30 18:51:58 +0100

    update

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
 fetch("data/posts.json")
   .then((response) => response.json())
   .then((json) => console.log(json.id));
+  
+
+const el = {
+  navmenu: document.querySelector(".navmenu"),
+  navmenu_toggle: document.querySelector(".navmenu-toggle")
+}
+
+el.navmenu_toggle.addEventListener("click", () => {
+  el.navmenu.classList.toggle("visible");
+})
