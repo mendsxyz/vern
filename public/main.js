19cc4072b8e5b4051d2277b46dc0f30ed50678d9
mendsxyz <princemendie03@gmail.com>
2024-12-01 08:29:37 +0100

    update

--- public/main.js
+++ public/main.js
@@ -45,20 +45,55 @@ function renderCards(data) {
   Object.values(data).forEach(obj => {
     const fypCardsBlob = forYouPost(obj);
     const favCardsBlob = favoritePost(obj);
-    
+
     tab.content.forEach(tab => {
       tab.dataset.tab == "for-you" ?
-      tab.insertAdjacentHTML("afterbegin", fypCardsBlob) :
-      tab.insertAdjacentHTML("afterbegin", favCardsBlob);
+        tab.insertAdjacentHTML("afterbegin", fypCardsBlob) :
+        tab.insertAdjacentHTML("afterbegin", favCardsBlob);
     })
   })
 }
 
 // Post card html
 
+let maxBodyLength = 59;
+
 function forYouPost(obj) {
   return `
-    
+    <div class="post-card w-full p-4 grid grid-flow-row content-center justify-start gap-2">
+      <div class="w-full flex items-center justify-left gap-2">
+        <img src="${obj.author_pfp}" alt="user profile icon" class="user-profile-icon">
+        <div class="author-post-info text-sm">${obj.author_name}</div>
+      </div>
+      <div class="w-full grid grid-flow-col items-center justify-start gap-4">
+        <div class="w-full grid grid-flow-row gap-2">
+          <div class="post-header text-2xl font-bold leading-none">${obj.header}</div>
+          <div class="post-body text-sm font-light leading-snug">${obj.body.length > maxBodyLength ? obj.body.substring(0, maxBodyLength) + "..." : obj.body}</div>
+        </div>
+        <img class="post-image" src="assets/post-img/post-img.png" alt="post image">
+      </div>
+      <div class="w-full flex items-center justify-between">
+        <div class="w-full flex items-center justify-left gap-4">
+          <div class="flex items-center justify-left gap-0">
+            <span class="ms-rounded xs">bar_chart</span>
+            <div class="text-xs font-light">${obj.impressions}</div>
+          </div>
+          <div class="flex items-center justify-left gap-1">
+            <span class="ms-rounded xs">thumb_up</span>
+            <div class="text-xs font-light">${obj.likes}</div>
+          </div>
+          <div class="flex items-center justify-left gap-0">
+            <span class="ms-rounded xs">bookmark</span>
+            <div class="text-xs font-light">${obj.favorites}</div>
+          </div>
+        </div>
+        <div class="items-center justify-left gap-0">
+          <div class="flex items-center justify-left">
+            <span class="add-to-favorites ms-rounded">add_circle</span>
+          </div>
+        </div>
+      </div>
+    </div>
   `
 }
 
